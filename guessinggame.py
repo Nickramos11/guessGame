24cfa0cd4fd2dcf218289ad57f42bd9f2c8fc872
Nicholas Ramos <nicholasramos@Nicholass-MacBook-Air-2.local>
2020-09-20 20:11:40 -0500

    Updated guessing game

--- guessinggame.py
+++ guessinggame.py
@@ -1,34 +1,31 @@
-answer = 5
+import random
 
-print("Please guess number between 1 and 10: ")
-guess = int(input())
 
-if guess == answer:
-    print("You got it first time")
-else:
-    if guess < answer:
-        print("Please guess higher")
-    else:   # guess must be greater than answer
-        print("Please guess lower")
-    guess = int(input())
+def get_integer(prompt):
+    while True:
+        temp = input(prompt)
+        if temp.isnumeric():
+            return int(temp)
+        # else:
+        print("{0} is not a valid number".format(temp))
+
+
+highest = 1000
+answer = random.randint(1, highest)
+print(answer) #TODO: remove after testing
+guess = 0 # initialise to any number that doesnt = answer
+print("Please guess an answer between 1 and {}: ".format(highest))
+
+while guess != answer:
+    guess = get_integer(": ")
+
+    if guess == 0:
+        break
     if guess == answer:
-        print("Well done, you guessed it")
+        print("You got it!")
+        break
     else:
-        print("Sorry, you have not guessed correctly")
-
-# if guess < answer:
-#     print("Please guess higher")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# elif guess > answer:
-#     print("Please guess lower")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# else:
-#     print("You got it first time")
+        if guess < answer:
+            print("please guess higher")
+        else:
+            print("Please guess lower")
